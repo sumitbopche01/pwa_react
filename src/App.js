5a9fe26e73635f0979d93167cd92070e4b42cc75
codeitdown <sumitbopche01@gmail.com>
2020-10-23 14:02:40 +0530

    input field, weather data integration with ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { fetchWeather } from './api/fetchWeather';
+import './App.css';
 
 const App = () => {
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState('');
+
+    const search = async (e) => {
+        if (e.key == 'Enter') {
+            const data = await fetchWeather(query);
+
+            setWeather(data);
+            setQuery('');
+
+        }
+    }
+
     return (
-        <h1>App</h1>
+        <div className='main_container'>
+            <input
+                type='text'
+                className='search'
+                placeholder='Search ...'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+
+            {weather.main && (
+                <div className="city">
+                    <h2 className='city-name'>
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+
+                    <div className='city-temp'>
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
